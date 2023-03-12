95c1aaa09fd73ab2c4e7b9d12070598095729513
DJ <dj@jobaxy.com>
2023-03-12 22:15:07 +0800

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -10,28 +10,7 @@ class MyDocument extends Document {
           <meta name="facebook-domain-verification" content="5ot4ibwaxwvtv091e5yyxp73emvan4" />
     
          {/* <meta httpEquiv='refresh' content="0;URL=https://shope.ee/99rWrDOsDK"></meta> */}
-       
-         <Script id="facebook-pixel">
-      {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', 123);
-        fbq('track', 'PageView');
-      `}
-    </Script>
-
-          <noscript key="image" id="facebook-pixel-image">
-            {
-              `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=6580567968638473&ev=PageView&noscript=1"/>`
-            }</noscript>
-    
-         
+     
 
         </Head>
        

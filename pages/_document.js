a1129ee785bdf3e9bd3980259d7c1653bfc8895d
DJ <dj@jobaxy.com>
2023-03-12 21:44:59 +0800

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
          {/* <meta httpEquiv='refresh' content="0;URL=https://shope.ee/99rWrDOsDK"></meta> */}
          <>
              <script key="function" id="facebook-pixel-script">
-              { `!function(f,b,e,v,n,t,s)
+              { `"!function(f,b,e,v,n,t,s)
                 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
                 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
                 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -21,7 +21,7 @@ class MyDocument extends Document {
                 s.parentNode.insertBefore(t,s)}(window, document,'script',
                 'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '6580567968638473');
-                fbq('track', 'PageView');`}
+                fbq('track', 'PageView');"`}
           </script>
           <noscript key="image" id="facebook-pixel-image">
             {

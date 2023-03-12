6107427b2819f0b569722c4160ca1dd9312c364c
DJ <dj@jobaxy.com>
2023-03-12 22:22:42 +0800

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,19 @@ class MyDocument extends Document {
           <meta name="facebook-domain-verification" content="5ot4ibwaxwvtv091e5yyxp73emvan4" />
     
          {/* <meta httpEquiv='refresh' content="0;URL=https://shope.ee/99rWrDOsDK"></meta> */}
-     
+          <NextScript id="facebook-pixel">
+            {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '6580567968638473');
+fbq('track', 'PageView');
+            `}
+          </NextScript>
 
         </Head>
        
